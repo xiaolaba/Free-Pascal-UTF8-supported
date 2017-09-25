2bbc3bee12a795a5c0cccdd8cc5d88df39702063
xiaolaba <xiao_laba_cn@yahoo.com>
2017-09-25 11:09:31 +0800

    Add files via upload

---
+++ utf8n.pas
@@ -0,0 +1,19 @@
+﻿{ref : http://wiki.freepascal.org/Console_Mode_Pascal#Unicode_.28UTF8.29_output}
+{實驗如何用 Free PASCAL 輸出 UTF8 字串, by xiaolaba}
+program uniconsole;
+{$mode objfpc}{$H+}
+{$APPTYPE CONSOLE}
+uses
+  Windows,Classes;
+var
+	UTF8TestString, UTF8TestString2: String;
+begin
+	{SetConsoleOutputCP(CP_ACP);}
+	
+	{set console code page to 65001 = UTF8}
+	SetConsoleOutputCP(CP_UTF8);
+	UTF8TestString:= '简体';
+	UTF8TestString2:= '繁體';
+	writeln ('plain: ' + UTF8TestString);
+	writeln ('plain: ' + UTF8TestString2);
+end.
